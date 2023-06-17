f8881b5dce09c33c25a3989ecc219b181c9b418b
karasandra <lykeny@yahoo.com>
2023-06-16 20:24:39 -0400

    Blood Touch ups

--- Wrath/src/main/java/org/kara/wrath/utility/Location.java
+++ Wrath/src/main/java/org/kara/wrath/utility/Location.java
@@ -9,6 +9,6 @@ public class Location {
     public static final Area MYTH_GUILD_UPPPER = new Area( new Tile(2445, 2855, 1), new Tile(2465, 2835, 1));
     public static final Area MYTH_GUILD_LOWER = new Area(new Tile(2452, 2852), new Tile(2462, 2838));
     public static final Area WRATH_ALTER = new Area(new Tile(2337, 4825), new Tile(2332, 4834));
-    public static final Area MYTH_ALTER = new Area(new Tile(2442, 2820, 0), new Tile(2448, 2817, 0));
+    public static final Area MYTH_ALTER = new Area(new Tile(2440, 2830, 0), new Tile(2455, 2815, 0));
     public static final Area MYTH_STAIRS = new Area(new Tile(2456, 2841), new Tile(2458, 2838));
 }
